f6f749615d15adca2e615e2580ea702c01d5dd9d
Alex-McAlpine <alexmcalpine7@hotmail.com>
2025-07-23 13:30:00 +0100

    UPDATED: Secret Key

--- env.py
+++ env.py
@@ -4,6 +4,4 @@ os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_4pQrE7PtFfhb@ep-steep-fog-a2ls59ip.eu-central-1.aws.neon.tech/video_gig_canon_167234"
 )
 
-os.environ.setdefault(
-    "SECRET_KEY", "fwhj8c&wl3pp5n@t#$vbbzw)8w-t8+0us*bcz7zus1a&kp$2_0"
-)
+os.environ.setdefault("SECRET_KEY", "fwhj8c&wl3pp5n@t#$vbbzw)8w-t8+0us*bcz7zus1a&kp$2_0")
